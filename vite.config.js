ae516883c063266e8d1b5c0e4c218cd8912ab233
Faizan Ashraf <hashmif747@gmail.com>
2023-12-01 02:51:08 +0500

    Update vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
+import { resolve } from "path"
 import {defineConfig} from 'vite'
 
 
 export default defineConfig({
-	plugins: []	
+	plugins: [],
+	build: {
+		rollupOptions: {
+		  input: {
+			main: resolve(__dirname, 'portfolio/index.html'),
+			nested: resolve(__dirname, 'portfolio/project-1.html'),
+			nested: resolve(__dirname, 'portfolio/project-2.html'),
+			nested: resolve(__dirname, 'portfolio/project-3.html'),
+			nested: resolve(__dirname, 'portfolio/project-4.html'),
+			nested: resolve(__dirname, 'portfolio/project-5.html'),
+			nested: resolve(__dirname, 'portfolio/project-6.html'),
+		  },
+		},
+	  },
 })
